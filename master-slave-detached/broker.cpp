429df8be36f7b2e7b43c081078179e3c6bbf9b21
Nicolò Tonci <nicolo.tonci@phd.unipi.it>
2023-04-04 14:27:28 +0200

    Get rid of the cmake files. Now using just GNU make. Fixed some relative typos to the examples

--- master-slave-detached/broker.cpp
+++ master-slave-detached/broker.cpp
@@ -4,13 +4,14 @@
 int main(int argc, char *argv[]) {
     Manager::init("Broker", "config.json");
 
-    auto master = Manager::connect("Master");
+    auto master = Manager::connect("TCP:Master");
     auto bcast = Manager::createTeam("Broker:Worker1:Worker2", "Broker", BROADCAST);
     auto gather = Manager::createTeam("Broker:Worker1:Worker2", "Broker", GATHER);
 
 
     size_t modelSize;
     master.receive(&modelSize, sizeof(size_t));
+    bcast.sendrecv(&modelSize, sizeof(size_t), nullptr, 0);
     char* buffer = new char[modelSize];
     char* backBuff = new char[modelSize * gather.size()];
     while(true){
@@ -19,16 +20,15 @@ int main(int argc, char *argv[]) {
 
         bcast.sendrecv(buffer, modelSize, nullptr, 0);
 
-        gather.sendrecv(backBuff, modelSize, backBuff, modelSize); // local send not meaningful, just skipped
+        gather.sendrecv(buffer, modelSize, backBuff, modelSize); // local send not meaningful, just skipped
 
         for(int i = 1; i < gather.size(); i++)
             master.send(backBuff+(i*modelSize), modelSize);
-
-        delete [] backBuff;
     }
-
+    delete [] backBuff;
+    delete [] buffer;
     master.close();
-    std::string eos("EOS", modelSize);
+    std::string eos(modelSize, 'E'); eos[1] = 'O'; eos[2] = 'S';
     bcast.sendrecv(eos.c_str(), modelSize, nullptr, 0);
     bcast.close();
     Manager::finalize(true);
